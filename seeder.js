1d1b2135a4cc3d51290c075cc6ed2a3d530551c4
Richard van Deursen <richard@whisperofcoding.com>
2022-08-13 23:20:20 +0200

    added Courses

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
  mongoose.connect(process.env.MONGO_URI, {
@@ -20,10 +21,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported successfully'.green.inverse);
     process.exit();
   } catch (error) {
@@ -35,6 +41,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
       await Bootcamp.deleteMany();
+      await Course.deleteMany();
       console.log('Data destroyed successfully'.red.inverse);
       process.exit();
     } catch (error) {
