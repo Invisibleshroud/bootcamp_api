8fa7ed67440b4147230f1367cee2ec5122aab92f
Richard van Deursen <richard@whisperofcoding.com>
2022-08-15 20:57:37 +0200

    Finalized Auth & added Users

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -26,17 +27,20 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please add a password'],
     minLength: 6,
     select: false,
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
 // Encrypte password using bcrypt
 UserSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) {
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -51,6 +55,22 @@ UserSchema.methods.getSignedJwtToken = function () {
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/courses.js
+++ routes/courses.js
@@ -9,11 +9,10 @@ const {
 
 const Course = require('../models/Course');
 
-const advancedResults = require('../middleware/advancedResults');
-
 const router = express.Router({ mergeParams: true });
 
-const { protect , authorize} = require('../middleware/auth');
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
 
 router
   .route('/')
@@ -24,11 +23,11 @@ router
     }),
     getCourses
   )
-  .post(protect, authorize('publisher', 'admin'),addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect,authorize('publisher', 'admin'), updateCourse)
-  .delete(protect, authorize('publisher', 'admin'),deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
